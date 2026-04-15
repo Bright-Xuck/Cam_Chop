cd80b641d581406c75643c3b4f65b0561ffb2fae
Bright-Xuck <brightnjikang@gmail.com>
2026-04-15 12:05:47 +0100

    last completley working checkpoint

--- src/pages/customer/auth/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import CustomerAuthLogin from "../Components/customer/auth/CustomerAuthLogin"
-
-export default function LoginPage({data}){
-    return(
-        <CustomerAuthLogin data={data} />
-    )
-}
